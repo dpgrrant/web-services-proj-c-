27d18a825120857b1ac9c410f1c78e2cfab34293
dpgrrant <80363299+dpgrrant@users.noreply.github.com>
2023-03-08 14:27:16 -0500

    Add files via upload

---
+++ InventoryCart/iOS/AppDelegate.fs
@@ -0,0 +1,23 @@
+﻿namespace InventoryCart.iOS
+
+open System
+open UIKit
+open Foundation
+open Xamarin.Forms
+open Xamarin.Forms.Platform.iOS
+
+[<Register ("AppDelegate")>]
+type AppDelegate () =
+    inherit FormsApplicationDelegate ()
+
+    override this.FinishedLaunching (app, options) =
+        Forms.Init()
+        this.LoadApplication (new InventoryCart.App())
+        base.FinishedLaunching(app, options)
+
+module Main =
+    [<EntryPoint>]
+    let main args =
+        UIApplication.Main(args, null, "AppDelegate")
+        0
+
